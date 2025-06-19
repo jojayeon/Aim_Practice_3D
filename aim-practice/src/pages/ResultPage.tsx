0d5817ce9e375936f9a4b20a88af497189a09cb8
jojayeon <jojayeon6152@gmail.com>
2025-06-19 17:05:34 +0900

    :pencil2: 콘솔 추가

--- aim-practice/src/pages/ResultPage.tsx
+++ aim-practice/src/pages/ResultPage.tsx
@@ -7,6 +7,7 @@ import styles from '../styles/ResultPage.module.css';
 const ResultPage: React.FC = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  console.log("📦 Received state from navigate:", location.state);
   const { score } = location.state || { score: 0 };
 
   return (
